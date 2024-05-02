0f77a29ca53488a2136f21d7b3fce0fee10033bf
ak <151400939+akiongithub@users.noreply.github.com>
2024-05-01 18:35:44 -0700

    Update directories.sh

--- directories.sh
+++ directories.sh
@@ -8,9 +8,10 @@ target=$(zenity --entry --text "What is your target website?" --title "Set Targe
 # Tidy up URL
 if [[ $target == http://* ]]; then
     target="${target#http://}"
-elif [[ $target== https://* ]] then
+elif [[ $target == https://* ]]; then
     target="${target#https://}"
-fi
+fi 
+
 if [[ $target == */ ]]; then
     target="${target%/}"
 fi
@@ -23,16 +24,6 @@ fi
 
 echo "You have chosen: $target"
 
-http_status=$(curl -s -o /dev/null -w "%{http_code}" "$protocol://$target")
-# Check if website needs authorized access
-if [[ "$http_status" == "401" ]] || [[ "$http_status" == "403" ]]; then
-    username=$(zenity --entry --text "What is the username?" --title "Set Username" 2>/dev/null)
-    password=$(zenity --entry --text "What is the password?" --title "Set Password" 2>/dev/null)
-    login="$username:$password@"
-else
-    login=""
-fi 
-
 # Prompt for protocol selection
 protocol=$(zenity --list --title="Select Protocol" --column="Protocol" "http" "https" 2>/dev/null)
 
@@ -42,8 +33,19 @@ if [ -z "$protocol" ]; then
     exit 1
 fi
 
+http_status=$(curl -s -o /dev/null -w "%{http_code}" "$target")
+# Check if website needs authorized access
+if [[ "$http_status" == "401" ]] || [[ "$http_status" == "403" ]]; then
+    username=$(zenity --entry --text "What is the username?" --title "Set Username" 2>/dev/null)
+    password=$(zenity --entry --text "What is the password?" --title "Set Password" 2>/dev/null)
+    login="$username:$password@"
+else
+    login=""
+fi 
 echo "Protocol selected: $protocol"
 echo "Initializing Fuzz"
 
 # Run ffuf for a short duration to gather initial data
 ffuf -u $protocol"://"$login$target"/FUZZ" -w ~/wordlists/common.txt -mc 100-299,500-599
+
+
